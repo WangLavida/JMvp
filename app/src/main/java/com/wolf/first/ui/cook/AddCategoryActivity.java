822e0510581c93f5a69c2f6dd91b01869598a4c4
阮阮的建建 <0706wolf@sina.com>
2017-06-07 16:37:04 +0800

    启动页

--- app/src/main/java/com/wolf/first/ui/cook/AddCategoryActivity.java
+++ app/src/main/java/com/wolf/first/ui/cook/AddCategoryActivity.java
@@ -19,12 +19,17 @@ import com.wolf.first.bean.CategoryInfoBean;
 import com.wolf.first.contract.AddCategoryContract;
 import com.wolf.first.model.AddCategoryModel;
 import com.wolf.first.presenter.AddCategoryPresenter;
+import com.wolf.first.util.MyLog;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.zip.Inflater;
 
 import butterknife.Bind;
+
+import static android.R.attr.x;
+import static android.R.id.list;
 
 public class AddCategoryActivity extends BaseActivity<AddCategoryPresenter, AddCategoryModel> implements AddCategoryContract.View {
 
@@ -64,7 +69,15 @@ public class AddCategoryActivity extends BaseActivity<AddCategoryPresenter, AddC
         allList = (List<CategoryInfoBean>) getIntent().getExtras().getSerializable(Constant.ALL_LIST_KEY);
         myList = (List<CategoryInfoBean>) getIntent().getExtras().getSerializable(Constant.MY_LIST_KEY);
         otherList.addAll(allList);
-        otherList.removeAll(myList);
+        Iterator<CategoryInfoBean> it = otherList.iterator();
+        while (it.hasNext()) {
+            CategoryInfoBean categoryInfoBean = it.next();
+            for (CategoryInfoBean categoryInfoBean1 : myList) {
+                if (categoryInfoBean.getName().equals(categoryInfoBean1.getName())) {
+                    it.remove();
+                }
+            }
+        }
     }
 
     @Override
@@ -135,7 +148,8 @@ public class AddCategoryActivity extends BaseActivity<AddCategoryPresenter, AddC
         });
         allRecyclerView.setAdapter(allAdapter);
     }
-    private void notifyDataSetChanged(){
+
+    private void notifyDataSetChanged() {
         allAdapter.notifyDataSetChanged();
         myAdapter.notifyDataSetChanged();
     }
